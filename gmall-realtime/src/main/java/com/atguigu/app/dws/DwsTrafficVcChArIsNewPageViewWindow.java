de8c05e5cb45518df63f37e1b4509c563e96e79b
whalex <whalex@atguigu.com>
2022-07-26 12:01:19 +0800

    day07(2)

---
+++ gmall-realtime/src/main/java/com/atguigu/app/dws/DwsTrafficVcChArIsNewPageViewWindow.java
@@ -0,0 +1,179 @@
+package com.atguigu.app.dws;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.atguigu.bean.TrafficPageViewBean;
+import com.atguigu.utils.DateFormatUtil;
+import com.atguigu.utils.MyKafkaUtil;
+import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple4;
+import org.apache.flink.streaming.api.datastream.*;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.util.Collector;
+
+import java.time.Duration;
+
+public class DwsTrafficVcChArIsNewPageViewWindow {
+
+    public static void main(String[] args) {
+
+        //TODO 1.获取执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        // 1.1 状态后端设置
+//        env.enableCheckpointing(3000L, CheckpointingMode.EXACTLY_ONCE);
+//        env.getCheckpointConfig().setCheckpointTimeout(60 * 1000L);
+//        env.getCheckpointConfig().setMinPauseBetweenCheckpoints(3000L);
+//        env.getCheckpointConfig().enableExternalizedCheckpoints(
+//                CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION
+//        );
+//        env.setRestartStrategy(RestartStrategies.failureRateRestart(
+//                3, Time.days(1), Time.minutes(1)
+//        ));
+//        env.setStateBackend(new HashMapStateBackend());
+//        env.getCheckpointConfig().setCheckpointStorage(
+//                "hdfs://hadoop102:8020/ck"
+//        );
+//        System.setProperty("HADOOP_USER_NAME", "atguigu");
+
+        //TODO 2.读取Kafka DWD层3个主题的数据创建流
+        String pageTopic = "dwd_traffic_page_log";
+        String ujdTopic = "dwd_traffic_user_jump_detail";
+        String uvTopic = "dwd_traffic_unique_visitor_detail";
+        String groupId = "page_view_220212";
+        DataStreamSource<String> pageLogDS = env.addSource(MyKafkaUtil.getFlinkKafkaConsumer(pageTopic, groupId));
+        DataStreamSource<String> uvLogDS = env.addSource(MyKafkaUtil.getFlinkKafkaConsumer(uvTopic, groupId));
+        DataStreamSource<String> ujLogDS = env.addSource(MyKafkaUtil.getFlinkKafkaConsumer(ujdTopic, groupId));
+
+        //TODO 3.将其转换为JavaBean对象
+        //3.1 处理UV数据
+        SingleOutputStreamOperator<TrafficPageViewBean> pageViewWithUvDS = uvLogDS.map(line -> {
+            JSONObject jsonObject = JSON.parseObject(line);
+            JSONObject common = jsonObject.getJSONObject("common");
+            return new TrafficPageViewBean("", "",
+                    common.getString("vc"),
+                    common.getString("ch"),
+                    common.getString("ar"),
+                    common.getString("is_new"),
+                    1L, 0L, 0L, 0L, 0L, jsonObject.getLong("ts"));
+        });
+
+        //3.2 处理UJ数据
+        SingleOutputStreamOperator<TrafficPageViewBean> pageViewWithUjDS = ujLogDS.map(line -> {
+            JSONObject jsonObject = JSON.parseObject(line);
+            JSONObject common = jsonObject.getJSONObject("common");
+            return new TrafficPageViewBean("", "",
+                    common.getString("vc"),
+                    common.getString("ch"),
+                    common.getString("ar"),
+                    common.getString("is_new"),
+                    0L, 0L, 0L, 0L, 1L, jsonObject.getLong("ts"));
+        });
+
+        //3.3 处理page_log数据
+        SingleOutputStreamOperator<TrafficPageViewBean> pageViewWithPvDS = pageLogDS.map(line -> {
+            JSONObject jsonObject = JSON.parseObject(line);
+            JSONObject common = jsonObject.getJSONObject("common");
+            JSONObject page = jsonObject.getJSONObject("page");
+
+            String lastPageId = page.getString("last_page_id");
+            long sv = 0L;
+            if (lastPageId == null) {
+                sv = 1L;
+            }
+
+            return new TrafficPageViewBean("", "",
+                    common.getString("vc"),
+                    common.getString("ch"),
+                    common.getString("ar"),
+                    common.getString("is_new"),
+                    0L,
+                    sv,
+                    1L,
+                    page.getLong("during_time"),
+                    0L,
+                    jsonObject.getLong("ts"));
+        });
+
+        //TODO 4.合并3个流
+        DataStream<TrafficPageViewBean> unionDS = pageViewWithUjDS.union(pageViewWithPvDS, pageViewWithUvDS);
+
+        //TODO 5.提取事件时间生成Watermark
+        SingleOutputStreamOperator<TrafficPageViewBean> pageViewWithWMDS = unionDS.assignTimestampsAndWatermarks(WatermarkStrategy.<TrafficPageViewBean>forBoundedOutOfOrderness(Duration.ofSeconds(2)).withTimestampAssigner(new SerializableTimestampAssigner<TrafficPageViewBean>() {
+            @Override
+            public long extractTimestamp(TrafficPageViewBean element, long recordTimestamp) {
+                return element.getTs();
+            }
+        }));
+
+        //TODO 6.分组、开窗、聚合
+        KeyedStream<TrafficPageViewBean, Tuple4<String, String, String, String>> keyedStream = pageViewWithWMDS.keyBy(new KeySelector<TrafficPageViewBean, Tuple4<String, String, String, String>>() {
+            @Override
+            public Tuple4<String, String, String, String> getKey(TrafficPageViewBean value) throws Exception {
+                return new Tuple4<>(value.getAr(),
+                        value.getCh(),
+                        value.getIsNew(),
+                        value.getVc());
+            }
+        });
+
+        WindowedStream<TrafficPageViewBean, Tuple4<String, String, String, String>, TimeWindow> windowedStream = keyedStream.window(TumblingEventTimeWindows.of(Time.seconds(10)));
+
+        //增量聚合:来一条聚合一条 ---> 计算的快,节省空间
+//        windowedStream.reduce(new ReduceFunction<TrafficPageViewBean>() {
+//            @Override
+//            public TrafficPageViewBean reduce(TrafficPageViewBean value1, TrafficPageViewBean value2) throws Exception {
+//                return null;
+//            }
+//        });
+        //全量聚合:攒着一起处理   ---> 百分比指标,排序,可以获取窗口信息
+//        windowedStream.apply(new WindowFunction<TrafficPageViewBean, TrafficPageViewBean, Tuple4<String, String, String, String>, TimeWindow>() {
+//            @Override
+//            public void apply(Tuple4<String, String, String, String> key, TimeWindow window, Iterable<TrafficPageViewBean> input, Collector<TrafficPageViewBean> out) throws Exception {
+//            }
+//        });
+
+        SingleOutputStreamOperator<TrafficPageViewBean> resultDS = windowedStream.reduce(new ReduceFunction<TrafficPageViewBean>() {
+            @Override
+            public TrafficPageViewBean reduce(TrafficPageViewBean value1, TrafficPageViewBean value2) throws Exception {
+                value1.setUvCt(value1.getUvCt() + value2.getUvCt());
+                value1.setUjCt(value1.getUjCt() + value2.getUjCt());
+                value1.setPvCt(value1.getPvCt() + value2.getPvCt());
+                value1.setSvCt(value1.getSvCt() + value2.getSvCt());
+                value1.setDurSum(value1.getDurSum() + value2.getDurSum());
+                return value1;
+            }
+        }, new WindowFunction<TrafficPageViewBean, TrafficPageViewBean, Tuple4<String, String, String, String>, TimeWindow>() {
+            @Override
+            public void apply(Tuple4<String, String, String, String> key, TimeWindow window, Iterable<TrafficPageViewBean> input, Collector<TrafficPageViewBean> out) throws Exception {
+
+                //获取数据
+                TrafficPageViewBean next = input.iterator().next();
+
+                //补充信息
+                next.setTs(System.currentTimeMillis());
+                next.setEdt(DateFormatUtil.toYmdHms(window.getEnd()));
+                next.setStt(DateFormatUtil.toYmdHms(window.getStart()));
+
+                //输出数据
+                out.collect(next);
+            }
+        });
+
+
+        //TODO 7.将数据写出到ClickHouse
+
+        //TODO 8.启动任务
+
+    }
+
+}
