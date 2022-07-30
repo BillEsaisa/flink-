1ec54c15d428ef5b6347797f1d3ac5ddb01b8702
whalex <whalex@atguigu.com>
2022-07-30 11:55:14 +0800

    day10(2)

--- gmall-realtime/src/main/java/com/atguigu/utils/DimUtil.java
+++ gmall-realtime/src/main/java/com/atguigu/utils/DimUtil.java
@@ -2,25 +2,42 @@ package com.atguigu.utils;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.pool.DruidPooledConnection;
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.atguigu.common.GmallConfig;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.List;
 
 public class DimUtil {
 
-    public static JSONObject getDimInfo(Connection connection, String tableName, String key) throws Exception {
+    public static JSONObject getDimInfo(Jedis jedis, Connection connection, String tableName, String key) throws Exception {
+
+        //读取Redis中的维表数据
+        String redisKey = "DIM:" + tableName + ":" + key;
+        String dimInfoStr = jedis.get(redisKey);
+        if (dimInfoStr != null) {
+            //重置过期时间
+            jedis.expire(redisKey, 24 * 60 * 60);
+            return JSON.parseObject(dimInfoStr);
+        }
 
         //拼接SQL
         String querySql = "select * from " + GmallConfig.HBASE_SCHEMA + "." + tableName + " where id='" + key + "'";
 
         //查询
         List<JSONObject> queryList = JdbcUtil.queryList(connection, querySql, JSONObject.class, false);
+        JSONObject dimInfo = queryList.get(0);
+
+        //将从Phoenix查询到的数据写入Redis
+        jedis.set(redisKey, dimInfo.toJSONString());
+        //设置过期时间
+        jedis.expire(redisKey, 24 * 60 * 60);
 
         //返回结果
-        return queryList.get(0);
+        return dimInfo;
     }
 
     public static void main(String[] args) throws Exception {
@@ -28,16 +45,21 @@ public class DimUtil {
         DruidDataSource dataSource = DruidDSUtil.createDataSource();
         DruidPooledConnection connection = dataSource.getConnection();
 
+        JedisPool jedisPool = JedisPoolUtil.getJedisPool();
+        Jedis jedis = jedisPool.getResource();
+
         long start = System.currentTimeMillis();
-        System.out.println(getDimInfo(connection, "DIM_BASE_TRADEMARK", "13")); //148 148 143 148 140
+        System.out.println(getDimInfo(jedis, connection, "DIM_BASE_TRADEMARK", "13")); //148 148 143 148 140
         long end = System.currentTimeMillis();
 
-        System.out.println(getDimInfo(connection, "DIM_BASE_TRADEMARK", "13")); //10 8 8 9
+        System.out.println(getDimInfo(jedis, connection, "DIM_BASE_TRADEMARK", "13")); //10 8 8 9   0 1 1 1 1
         long end2 = System.currentTimeMillis();
 
         System.out.println(end - start);
         System.out.println(end2 - end);
 
+        jedis.close();
+        jedisPool.close();
         connection.close();
         dataSource.close();
     }
---
+++ gmall-realtime/src/main/java/com/atguigu/utils/JedisPoolUtil.java
@@ -0,0 +1,30 @@
+package com.atguigu.utils;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+public class JedisPoolUtil {
+
+    private static JedisPool jedisPool;
+
+    private static void initJedisPool() {
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxTotal(100);
+        poolConfig.setMaxIdle(5);
+        poolConfig.setMinIdle(5);
+        poolConfig.setBlockWhenExhausted(true);
+        poolConfig.setMaxWaitMillis(2000);
+        poolConfig.setTestOnBorrow(true);
+        jedisPool = new JedisPool(poolConfig, "hadoop102", 6379, 10000);
+    }
+
+    public static JedisPool getJedisPool() {
+        if (jedisPool == null) {
+            initJedisPool();
+        }
+        // 获取Jedis客户端
+        return jedisPool;
+    }
+
+}
